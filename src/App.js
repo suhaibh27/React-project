d5855df4eb36eaa79708f3c20b822e541aa63d2a
Suhaib <suhaibhk@gmail.com>
2020-11-07 18:35:51 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
+//any js logic is written between {};
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//importing a component 
+import {CardList} from './Components/cardList/card-list.component';
+import { Search } from './Components/search/search';
+
+class App extends Component {
+  constructor()
+  {
+    super();
+    //the state which defines the what happen to the page
+    this.state={
+      monsters:[],
+      searchField:''
+    }
+
+  }
+
+  //one of lifecycle method of rendering 
+  //after react deploy the dom to page this function is called 
+  //where you use API to get data
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response=>response.json())
+          .then(users=>this.setState({monsters:users}));
+          
+  }
+  //handle the change when you type in search box
+  handleChange=e=>this.setState({searchField: e.target.value})
+
+  //this will define the structure of the component (the whole page).
+  //the tags are JSX NOT HTML
+  render(){
+    // const that have the states
+    // const monsters=this.state.monsters
+    const{monsters,searchField}=this.state;
+    const FilteredMonsters=monsters.filter(monster=>
+                                          monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+      <div className="App">
+        <Search placeHolder='Search Users' handleChange={this.handleChange}/>
+        {/* using the component like dom and passing data as props to be handeled by the component {CardList} */}
+        <CardList monsters={FilteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/search/search.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Search=({placeHolder,handleChange})=>(
+    <input type='search' placeholder={placeHolder} onChange={handleChange}/>
+)
