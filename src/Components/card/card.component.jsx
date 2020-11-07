ca6a18322a310f2cc2e4d11c7ea6894dc469a1e2
Suhaib <suhaibhk@gmail.com>
2020-11-07 19:26:01 +0300

    Adding address to card component

--- src/Components/card/card.component.jsx
+++ src/Components/card/card.component.jsx
@@ -6,5 +6,6 @@ export const Card=props=>(
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
+        <p>Address: Bahrain</p>
     </div>
 )
