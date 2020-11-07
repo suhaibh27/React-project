2caab4164f0b81442455ed0ab8a5821b08577bdb
unknown <anas.basha91@hotmail.com>
2020-11-07 11:12:17 -0500

    adding phone number to Card Component

--- src/Components/card/card.component.jsx
+++ src/Components/card/card.component.jsx
@@ -6,5 +6,6 @@ export const Card=props=>(
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
+        <p>Phone Number: 810-555-5555</p>
     </div>
 )
